9be5e3888f4df1964c14616097e91800d7091072
Mathieu Dery <mathieu.dery@outlook.fr>
2023-11-07 13:57:30 -0500

    fix memory leaks in cli

--- cli/src/udp_send.c
+++ cli/src/udp_send.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #define MAXLINE 1024
 
@@ -21,20 +22,38 @@ void *udp_send(const char *const host, const uint16_t port, const char *const cm
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(port);
     servaddr.sin_addr.s_addr = inet_addr(host);
+
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100000;
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+        close(sockfd);
+        return NULL;
+    }
        
     int n;
-    socklen_t len;
+    socklen_t len = sizeof(servaddr);
        
     n = sendto(sockfd, cmd, strlen(cmd), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
     
     if (n < 0) {
+        close(sockfd);
         return NULL;
     }
 
     void *buffer = malloc(MAXLINE);
+
+    if (buffer == NULL) {
+        close(sockfd);
+        return NULL;
+    }
+
     n = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
 
     if (n < 0) {
+        close(sockfd);
+        free(buffer);
         return NULL;
     }
 
